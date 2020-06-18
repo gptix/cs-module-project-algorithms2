224a812c3a959f71ed2294fa10820f3d2946ecdc
Jud Taylor <jud.taylor@protonmail.com>
2020-06-18 17:35:13 -0400

    coooookkiiiiiiiees

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,23 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(n, cache = None):
+    
+    if n < 0:
+        return 0
+    
+    if not cache:
+        cache = {0: 1, 1: 1, 2: 2}
 
-    pass
+    if n in cache:
+        return cache[n]
+    
+    # a NEW one!
+    foo = eating_cookies(n-3, cache) + eating_cookies(n-2, cache) + eating_cookies(n-1, cache)
+    cache[n] = foo
+    return foo
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
-
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    for i in range (7):
+        print(f"There are {eating_cookies(i)} ways for Cookie Monster to each {i} cookies")
